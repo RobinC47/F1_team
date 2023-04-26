c89cfc302b3d6db6e1f201a9027d9557afb35435
Zantwerk <84845271+Zantwerk@users.noreply.github.com>
2023-04-26 11:08:10 +0200

    Add files via upload

---
+++ F1_team/src/Mecanician.java
@@ -0,0 +1,35 @@
+public class Mechanician {
+    private String name;
+    private String position;
+    private int car;
+
+    public Mechanician(String name, String position, int car) {
+        this.name = name;
+        this.position = position;
+        this.car = car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getCar() {
+        return car;
+    }
+
+    public void setCar(int car) {
+        this.car = car;
+    }
+}
